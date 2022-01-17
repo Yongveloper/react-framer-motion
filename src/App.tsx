461c873a5480637887b3328348fe2143a4cc833d
Yongveloper <aaa9015@naver.com>
2022-01-17 21:55:23 +0900

    MotionValues part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-import { motion, Variants } from 'framer-motion';
-import { useRef } from 'react';
+import { motion, useMotionValue, Variants } from 'framer-motion';
+import { useEffect } from 'react';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -28,26 +28,12 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants: Variants = {
-  hover: { rotateZ: 90 },
-  click: { borderRadius: '100px' },
-};
-
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          dragElastic={0.5}
-          dragConstraints={biggerBoxRef}
-          variants={boxVariants}
-          whileHover="hover"
-          whileTap="click"
-        />
-      </BiggerBox>
+      <Box style={{ x }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
